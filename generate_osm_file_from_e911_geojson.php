47a4563f9db9a5dba59dda5cc387e0ddf5b1068c
Adam Franco <afranco@middlebury.edu>
2025-08-18 14:23:43 -0400

    Add case for "V.A. Cutoff Road"

--- generate_osm_file_from_e911_geojson.php
+++ generate_osm_file_from_e911_geojson.php
@@ -668,6 +668,12 @@ function normalize_street_base_name($street_name) {
         $street_name_title_cased = "Truck Route";
     }
 
+    // Hartford has a road called "V.A. Cutoff Road".
+    // (esiteid: 122757)
+    if ($street_name_title_cased == "Va Cutoff") {
+        $street_name_title_cased = "V.A. Cutoff";
+    }
+
     // Fairfield has 3 roads like "Napoli Road 1", "Napoli Road 2", and "Napoli Road 3" in E911.
     // The first first should probably be "Napoli Camp Road" and the later two "Napoli Road".
     // (esiteid: 104494, 104507, 104520)
